8d6fce438b7a32b8b912cffa6d6a7fd7b6beaf94
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2022-03-29 19:16:17 +0600

    install react router dom

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Card, Form, Button } from 'react-bootstrap';
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+// import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 
 
